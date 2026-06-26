e9887d4f283c2749bf5c9afb2e9924cf40390b95
MUHAMMAD ADHIL <muhammadadhil.personal@gmail.com>
2026-06-26 22:58:47 +0530

    Production deployment ready

--- catering-backend/config/settings.py
+++ catering-backend/config/settings.py
@@ -110,12 +110,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'CONN_MAX_AGE': 600,
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
